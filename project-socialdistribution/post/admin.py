5078e53000c3cca58bd0e4dca62e5828b53294e8
hongbozhong <hzhong1@ualberta.ca>
2021-11-27 03:58:29 +0800

    comment model

--- project-socialdistribution/post/admin.py
+++ project-socialdistribution/post/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Post
+from .models import Post, Comment
 
 admin.site.register(Post)
+admin.site.register(Comment)
+
+class CommentInline(admin.StackedInline): 
+    model = Comment
+    extra = 0
+
+class ArticleAdmin(admin.ModelAdmin): 
+    inlines = [
+        CommentInline,
+    ]
--- project-socialdistribution/post/models.py
+++ project-socialdistribution/post/models.py
@@ -15,3 +15,14 @@ class Post(models.Model):
     
     def get_absolute_url(self):
         return reverse('post:post_detail', args=[str(self.id)])
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete = models.CASCADE)
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    text = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.text
+    
+    def get_absolute_url(self):
+        return reverse('post:post_list')
